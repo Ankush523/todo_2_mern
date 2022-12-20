a1c5e672843192c66d5301e322c9451d0c25a66c
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-20 12:14:38 +0530

    server setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose
+  .connect("mongodb://localhost:27017/usersdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
+
+app.listen(3001, () => console.log("Server started on port 3001"));
