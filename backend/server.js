d88a0f4a8bda470686b491d45da90fe6d0be4d3c
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-20 22:01:47 +0530

    aligned

--- backend/server.js
+++ backend/server.js
@@ -13,33 +13,33 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
-const Todo = require('./models/Todo')
+const Todo = require("./models/Todo");
 
-app.get('/todos', async (req,res) => {
-    const Todos = await Todo.find()
-    res.json(todos)
-})
+app.get("/todos", async (req, res) => {
+  const Todos = await Todo.find();
+  res.json(todos);
+});
 
-app.post('/todo/new', (req,res) => {
-    const todo = new todo({
-        text : req.body.text
-    });
+app.post("/todo/new", (req, res) => {
+  const todo = new todo({
+    text: req.body.text,
+  });
 
-    todo.save()
-    res.json(todo)
-})
+  todo.save();
+  res.json(todo);
+});
 
-app.delete('/todo/delete/:id', async (req,res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id)
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
 
-    res.json(result)
-})
+  res.json(result);
+});
 
-app.put('/todo/complete/:id', async (req,res) => {
-    const todo = await Todo.findById(req.params.id)
+app.put("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
 
-    todo.complete = !todo.complete
-    todo.save()
-    res.json(todo)
-})
+  todo.complete = !todo.complete;
+  todo.save();
+  res.json(todo);
+});
 app.listen(3001, () => console.log("Server started on port 3001"));
