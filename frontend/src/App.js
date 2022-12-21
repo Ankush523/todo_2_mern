53cac7edfbd1e672bbef56a73f871172df5dc481
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-21 10:16:09 +0530

    styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [todos, setTodos] = useState([]);
+  const [popupactive, setPopupactive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  const GetTodos = () => {
+    fetch(APIbase + "/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("error: ", err));
+  };
+  return <div className="App"></div>;
 }
 
 export default App;
